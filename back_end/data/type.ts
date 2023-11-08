b7202343d9e146bbbd16bab67a67ff8ccd0d79e1
thascoet <thascoet3@gmail.com>
2023-11-08 18:16:25 +0100

    add data types

---
+++ back_end/data/type.ts
@@ -0,0 +1,75 @@
+export type CardType =
+    | "Raw Material"
+    | "Manufactured Goods"
+    | "Civilian Structures"
+    | "Commercial Structures"
+    | "Military Structures"
+    | "Scientific Structures"
+    | "Guilds";
+
+export type Ressource =
+    | "gold"
+    | "wood"
+    | "stone"
+    | "clay"
+    | "ore"
+    | "glass"
+    | "loom"
+    | "papyrus";
+
+export type ScientificRessource = "compas" | "tablet" | "wheel";
+
+export type Action = "build" | "wonder" | "sell";
+
+export type Side = "day" | "night";
+
+export type MilitaryToken =
+    | "age1Victory"
+    | "age2Victory"
+    | "age3Victory"
+    | "defeat";
+
+export type Cost = null | { [key in Ressource]?: number };
+
+export type Card = {
+    id: number;
+    name: string;
+    type: CardType;
+    imgUrl: string;
+    cost: Cost;
+    chain: number | null;
+    effect: number;
+    chainsProvided: number[];
+};
+
+export type Wonder = {
+    id: number;
+    name: string;
+    side: Side;
+    startingRessource: Ressource;
+    stages: {
+        cost: Cost;
+        effect: number;
+    };
+};
+
+export type Board = {
+    wonder: number;
+    wonderStagesBuilded: boolean[];
+    cardsBuilded: number[];
+    gold: number;
+    militaryTokens: { [key in MilitaryToken]: number };
+    scientificsRessources: { [key in ScientificRessource]: number };
+    ressorcesAvaliable;
+};
+
+export type Turn = {
+    cardPlayed: number;
+    cards: number[];
+    action: Action;
+    ressource: {
+        self: Cost;
+        left: Cost;
+        right: Cost;
+    };
+};
